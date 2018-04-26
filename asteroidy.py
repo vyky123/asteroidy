bb55eabd3b192e46216ff32710e1150d920a09e0
Tomáš Vykydal <vyk35227@spseol.cz>
2018-04-26 14:07:23 +0200

    init commit

---
+++ asteroidy.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 26 13:28:07 2018
+
+@author: vyk35227
+"""
+
+import pyglet
+
+window = pyglet.window.Window()
+pyglet.app.run()
+print('Hotovo!')
